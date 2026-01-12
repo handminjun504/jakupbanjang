73d93ecc01c01cfc36c76462374530a8a7d61011
Donggeon <ehdrjs14514@gmail.com>
2026-01-12 14:43:22 +0900

    feat: ?꾩옣 ?좏깮 ?섏씠吏 濡쒕뵫 媛쒖꽑
    - ?덉씠?꾩썐(?쒕ぉ, ?ㅻ챸)??利됱떆 ?쒖떆
    - ?꾩옣 紐⑸줉 濡쒕뵫 以묒뿉留?InlineLoader ?쒖떆
    - ?섏씠吏媛 ??鍮좊Ⅴ寃??섑??섎뒗 寃껋쿂??媛쒖꽑
    - ?ъ슜??寃쏀뿕 ?μ긽

--- client/src/pages/foreman/SiteSelectionPage.tsx
+++ client/src/pages/foreman/SiteSelectionPage.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { getSites } from '../../api/foreman';
 import { useSiteStore } from '../../store/siteStore';
 import { theme } from '../../styles/theme';
+import InlineLoader from '../../components/common/InlineLoader';
 
 interface Site {
   id: number;
@@ -42,40 +43,30 @@ const SiteSelectionPage: React.FC = () => {
     navigate('/foreman/tasks');
   };
 
-  if (loading) {
-    return (
-      <Container>
-        <Card>
-          <LoadingText>현장 목록을 불러오는 중...</LoadingText>
-        </Card>
-      </Container>
-    );
-  }
-
-  if (error) {
-    return (
-      <Container>
-        <Card>
-          <ErrorText>{error}</ErrorText>
-          <RetryButton onClick={fetchSites}>다시 시도</RetryButton>
-        </Card>
-      </Container>
-    );
-  }
-
   return (
     <Container>
       <Card>
         <Title>현장 선택</Title>
         <Description>작업할 현장을 선택해주세요</Description>
         
-        {sites.length === 0 ? (
+        {loading ? (
+          // 로딩 중: 레이아웃은 바로 표시하고 인라인 로더만 표시
+          <InlineLoader message="현장 목록을 불러오는 중..." height="300px" />
+        ) : error ? (
+          // 에러 발생
+          <ErrorContainer>
+            <ErrorText>{error}</ErrorText>
+            <RetryButton onClick={fetchSites}>다시 시도</RetryButton>
+          </ErrorContainer>
+        ) : sites.length === 0 ? (
+          // 현장 없음
           <EmptyMessage>
             <EmptyIcon>📋</EmptyIcon>
             <EmptyText>등록된 현장이 없습니다.</EmptyText>
             <EmptySubText>관리자에게 문의하세요.</EmptySubText>
           </EmptyMessage>
         ) : (
+          // 현장 목록 표시
           <SiteList>
             {sites.map((site) => (
               <SiteCard key={site.id} onClick={() => handleSiteSelect(site)}>
@@ -324,15 +315,12 @@ const EmptySubText = styled.p`
   }
 `;
 
-const LoadingText = styled.p`
+const ErrorContainer = styled.div`
   text-align: center;
-  font-size: 1rem;
-  color: ${theme.colors.text.secondary};
-  padding: ${theme.spacing.xl};
+  padding: ${theme.spacing.xl} ${theme.spacing.md};
   
   @media (min-width: ${theme.breakpoints.tablet}) {
-    font-size: 1.125rem;
-    padding: 3rem;
+    padding: 3rem 1rem;
   }
 `;
 
